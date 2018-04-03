3c80d423a2b8ccec8419049afc117db61e6b243f
Bas Buller <m.b.buller@student.tudelft.nl>
2018-04-03 21:50:18 +0200

    nieuwe poging

--- leo_bas_new/orange_avoider.c
+++ leo_bas_new/orange_avoider.c
@@ -122,6 +122,7 @@ void orange_avoider_init()
  */
 void orange_avoider_periodic()
 {
+  VERBOSE_PRINT("Vision vector: [%d, %d, %d, %d, %d] \n", vision_vector[0], vision_vector[1], vision_vector[2], vision_vector[3], vision_vector[4]);
   VERBOSE_PRINT("Color counts: \n [%d, %d, %d, %d, %d] \n [%d, %d, %d, %d, %d] \n [%d, %d, %d, %d, %d] \n", color_count_cells[2], color_count_cells[5], color_count_cells[8], color_count_cells[11], color_count_cells[14]
 																										 , color_count_cells[1], color_count_cells[4], color_count_cells[7], color_count_cells[10], color_count_cells[13]
 																										 , color_count_cells[0], color_count_cells[3], color_count_cells[6], color_count_cells[9], color_count_cells[12]);
