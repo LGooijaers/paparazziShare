14476206a33a6a09c306a24a8260d50ee4a1aafe
Bas Buller <m.b.buller@student.tudelft.nl>
2018-04-04 23:18:50 +0200

    ik heb geen ruggengraat

--- vallen_en_opstaan/orange_avoider.c
+++ vallen_en_opstaan/orange_avoider.c
@@ -429,7 +429,7 @@ void movementNoHeading(float velocity)
 {
   moveWaypointForward(WP_GOAL, velocity);
   moveWaypointForward(WP_TRAJECTORY, VEL_SCALING * velocity);
-  nav_set_heading_towards_waypoint(WP_GOAL);
+  // nav_set_heading_towards_waypoint(WP_GOAL);
   chooseIncrementAvoidance();
   VERBOSE_PRINT("Changing heading \n");
 }
